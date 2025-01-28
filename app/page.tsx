a07dea6534a8cc48ebfe78f777ce7bc4d1f118d7
Rishabh <dplayarrr@gmail.com>
2025-01-28 15:30:38 +0530

    ADD: Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,211 @@
-import Image from "next/image";
+import Header from "@/components/header";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  ArrowRight,
+  ChevronRight,
+  Circle,
+  CirclePlus,
+  Copy,
+  Github,
+  Heart,
+  Linkedin,
+} from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section>
+      <Header />
+      <section className="mt-4 bg-mainbg rounded-lg p-2 text-gray-300">
+        <div className="flex justify-between items-center p-4">
+          <div>
+            <h1 className="text-xl font-semibold">I&apos;m Rishabh Chauhan</h1>
+            <p className="text-sm mt-2">Software Engineer from Delhi, IN.</p>
+            <p className="text-sm">Currently working at Deloitte.</p>
+            <div className="flex gap-4 mt-4">
+              <Button
+                size="sm"
+                className="p-2 text-xs font-semibold bg-secondbg text-white rounded-md hover:bg-mainbg"
+              >
+                <CirclePlus className="mb-1" />
+                Hire Me
+              </Button>
+              <Button
+                variant="ghost"
+                size="sm"
+                className="p-2 text-xs font-semibold text-white rounded-md border border-mainborder"
+              >
+                <Copy className="mb-1" />
+                Copy Email
+              </Button>
+            </div>
+          </div>
+          <Avatar className="w-32 h-32 ring-2">
+            <AvatarImage src="dp.png" />
+            <AvatarFallback>DP</AvatarFallback>
+          </Avatar>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="mt-8 rounded-lg bg-medbg p-4">
+          <div className="flex justify-between items-center">
+            <h1 className="flex gap-2 items-baseline">
+              <Circle className="w-2 h-2" />
+              Projects
+            </h1>
+            <Button
+              size="sm"
+              className="p-2 text-xs font-semibold bg-secondbg text-white rounded-md hover:bg-mainbg"
+            >
+              View All
+              <ArrowRight className="mb-1" />
+            </Button>
+          </div>
+          <div className="bg-secondbg p-4 rounded-lg mt-4 flex justify-between items-center">
+            <div className="flex gap-6">
+              <Avatar className="ring-4 ring-medbg">
+                <AvatarImage src="dp.png" />
+                <AvatarFallback>DP</AvatarFallback>
+              </Avatar>
+              <div>
+                <h1 className="text-lg font-semibold">Wirejoin</h1>
+                <p className="text-sm text-gray-400">
+                  CRM for Internet Service Providers
+                </p>
+              </div>
+            </div>
+            <ChevronRight className="text-gray-500" />
+          </div>
+          <div className="bg-secondbg p-4 rounded-lg mt-2 flex justify-between items-center">
+            <div className="flex gap-6">
+              <Avatar className="ring-4 ring-medbg">
+                <AvatarImage src="dp.png" />
+                <AvatarFallback>DP</AvatarFallback>
+              </Avatar>
+              <div>
+                <h1 className="text-lg font-semibold">Budgeteer</h1>
+                <p className="text-sm text-gray-400">
+                  Finance Management System
+                </p>
+              </div>
+            </div>
+            <ChevronRight className="text-gray-500" />
+          </div>
+          <div className="bg-secondbg p-4 rounded-lg mt-2 flex justify-between items-center">
+            <div className="flex gap-6">
+              <Avatar className="ring-4 ring-medbg">
+                <AvatarImage src="dp.png" />
+                <AvatarFallback>DP</AvatarFallback>
+              </Avatar>
+              <div>
+                <h1 className="text-lg font-semibold">3D Portfolio</h1>
+                <p className="text-sm text-gray-400">
+                  The Interesting Portfolio
+                </p>
+              </div>
+            </div>
+            <ChevronRight className="text-gray-500" />
+          </div>
+        </div>
+        <div className="mt-4 rounded-lg bg-medbg p-4">
+          <h1 className="flex gap-2 items-baseline">
+            <Circle className="w-2 h-2" />
+            Experience
+          </h1>
+          <div className="bg-secondbg p-4 rounded-lg mt-4">
+            <div className="flex justify-between items-start">
+              <div className="flex gap-6">
+                <Avatar className="ring-4 ring-medbg">
+                  <AvatarImage src="dp.png" />
+                  <AvatarFallback>DP</AvatarFallback>
+                </Avatar>
+                <div>
+                  <h1 className="text-lg font-semibold">Deloitte</h1>
+                  <p className="text-sm text-gray-400">
+                    Software Engineer - II
+                  </p>
+                </div>
+              </div>
+              <div className="text-sm text-right">
+                <p>Jan 2022 - Present</p>
+                <p>Pune, IN</p>
+              </div>
+            </div>
+            <ul className="mt-4 text-sm list-disc ml-4">
+              <li>
+                Partnered with industry leaders like Google, Western Union, and
+                Veralto.
+              </li>
+              <li>
+                Directed a team of five engineers to deliver feature-rich web
+                applications showcased at Google Cloud Next 23 and CES 2024.
+              </li>
+              <li>Optimized applications serving over 1 million users.</li>
+            </ul>
+          </div>
+          <div className="bg-secondbg p-4 rounded-lg mt-2">
+            <div className="flex justify-between items-start">
+              <div className="flex gap-6">
+                <Avatar className="ring-4 ring-medbg">
+                  <AvatarImage src="dp.png" />
+                  <AvatarFallback>DP</AvatarFallback>
+                </Avatar>
+                <div>
+                  <h1 className="text-lg font-semibold">GGSIPU</h1>
+                  <p className="text-sm text-gray-400">
+                    Bachelor of Technology, IT
+                  </p>
+                </div>
+              </div>
+              <div className="text-sm text-right">
+                <p>Aug 2018 - Jul 2022</p>
+                <p>Delhi, IN</p>
+              </div>
+            </div>
+            <ul className="mt-4 text-sm list-disc ml-4">
+              <li>Graduated with a CGPA of 8.97</li>
+              <li>
+                Collaborated on the development of a MERN-based student
+                attendance tracking system for the university.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="p-8 flex flex-col justify-center items-center space-y-2">
+          <h1 className="text-2xl font-bold text-white">
+            Let&apos;s work together.
+          </h1>
+          <p>Creating awesome user experiences</p>
+          <div className="flex gap-4">
+            <Button
+              size="sm"
+              className="p-2 mt-2 text-xs font-semibold bg-secondbg text-white rounded-md hover:bg-mainbg"
+            >
+              <CirclePlus className="mb-1" />
+              Hire Me
+            </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="p-2 mt-2 text-xs font-semibold text-white rounded-md border border-mainborder"
+            >
+              <Copy className="mb-1" />
+              Copy Email
+            </Button>
+          </div>
+        </div>
+        <div className="bg-medbg p-4 flex items-center justify-between rounded-lg">
+          <h1 className="flex gap-2 items-baseline">
+            <Circle className="w-2 h-2" />
+            Follow me
+          </h1>
+          <div className="flex gap-4">
+            <Github />
+            <Linkedin />
+          </div>
+        </div>
+        <div className="mt-2 bg-medbg flex justify-center items-center p-8 rounded-lg text-xs gap-1">
+          Made by Rishabh with <Heart className="w-3 h-3 mb-1" />
+        </div>
+      </section>
+    </section>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,41 @@
+import { CirclePlus, CircleUserRound, House } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export default function Header() {
+  return (
+    <section className="rounded-lg bg-mainbg p-2 border border-mainborder text-gray-300 flex justify-between">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                <House />
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/about" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                <CircleUserRound />
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div>
+        <Button className="p-2 text-xs font-semibold bg-secondbg text-white rounded-lg hover:bg-mainbg">
+          <CirclePlus className="mb-1" />
+          Hire Me
+        </Button>
+      </div>
+    </section>
+  );
+}
